504a062895f2b0f859f430c642483d2f24dc5ce6
Devonltaylor <devonltaylor22@gmail.com>
2018-03-18 23:16:14 -0400

    4th Commit: Java file finished, button connected and working. Try it out! Download run, hit eat cookie!

--- app/src/main/java/com/vonlor/androidcookie/MainActivity.java
+++ app/src/main/java/com/vonlor/androidcookie/MainActivity.java
@@ -14,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // This will Change the image from img1 to img2
+        ImageView cookieImageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        cookieImageView.setImageResource(R.drawable.after_cookie);
+
+        //This will change text from text1 to text2
+        TextView statusTextView = (TextView) findViewById(R.id.status_text_view);
+        statusTextView.setText("I'm so full!");
+    }
 }
