6af5ec381bde0c2bba0cfb46b3a60ccf8e241e22
Devonltaylor <devonltaylor22@gmail.com>
2018-03-18 22:58:24 -0400

    3rd Commit: Style set, gradle sync finished, sample photos added. Working on Java file.

--- app/src/main/java/com/vonlor/androidcookie/MainActivity.java
+++ app/src/main/java/com/vonlor/androidcookie/MainActivity.java
@@ -2,6 +2,9 @@ package com.vonlor.androidcookie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
